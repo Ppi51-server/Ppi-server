49979ac39fe086eeeffad1f7f8af3e0f373c6f09
Ppi51-server <pradumisankar007@gmail.com>
2026-09-17 13:18:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+// Root test route
+app.get('/', (req, res) => {
+    res.send('Diablo-Z Powerful Backend is Running Live!');
+});
+
+// Live Sync Data Route
 app.get('/get-data', (req, res) => {
     res.json({
         round: "20260917001",
         prediction: "Green",
         time: new Date().toLocaleTimeString()
     });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
